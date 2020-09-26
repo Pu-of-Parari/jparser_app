73a917e3426aeb3dce180b9ffe0bb1419dadde91
mae <tatsu.m1969s@gmail.com>
2020-09-26 18:51:39 +0900

    [Add] front/app added

---
+++ frontend/app/src/App.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import './App.css';
+import Axios from 'axios';
+
+//function App() {
+export class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: ''};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>text parser</h1>
+          <form onSubmit={this.handleSubmit}>
+            <label>
+              input text >>
+              <input type="text" value={this.state.value} onChange={this.handleChange} />
+            </label>
+            <input type="submit" value="Parse" />
+          </form>
+        </header>
+      </div>
+    );
+  }
+
+
+  wakati = text => {
+    //console.log("input text >>"+text)
+    Axios.post('http://127.0.0.1:5000/wakati', {
+      post_text: text
+    }).then(function(res) {
+      alert(res.data.result);
+    })
+  };
+
+  handleSubmit = event => {
+    this.wakati(this.state.value)
+    event.preventDefault();
+  };
+
+  handleChange = event => {
+    this.setState({ value: event.target.value });
+  };
+}
+
+export default App;
